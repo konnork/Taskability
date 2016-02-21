ccd59b2ca4e1a744e3eaad4d243627ec4c3dd0c8
Connor Krupp <connorkrupp@icloud.com>
2016-02-21 16:04:51 -0500

    Initial Commit

---
+++ Weight Tracker/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Weight Tracker
+//
+//  Created by Connor Krupp on 21/02/2016.
+//  Copyright © 2016 Connor Krupp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
