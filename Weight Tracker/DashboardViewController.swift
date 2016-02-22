345a19494ff8b3b86493ab98d966c3585ffcf781
Connor Krupp <connorkrupp@icloud.com>
2016-02-21 22:07:23 -0500

    Added types and UICollectionViewDataSource to DashboardVC

--- Weight Tracker/DashboardViewController.swift
+++ Weight Tracker/DashboardViewController.swift
@@ -7,12 +7,33 @@
 //
 
 import UIKit
+import WeightKit
 
 class DashboardViewController: UICollectionViewController {
 
+    // MARK: Types
+
+    struct MainStoryboard {
+        struct CollectionViewCellIdentifiers {
+            static let weightCell = "weightCell"
+        }
+    }
+
+    // MARK: Properties
+
+    /// Temporarily initialized as array
+    ///   will be more complex with iCloud/Healthkit
+    var weightData = [WeightItem]()
+
+    // MARK: ViewController Life Cycle
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        for mass in 60..<100 {
+            weightData.append(WeightItem(mass: Double(mass)))
+        }
+
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +41,18 @@ class DashboardViewController: UICollectionViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    // MARK: UICollectionViewDataSource
 
+    override func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return weightData.count
+    }
+
+    override func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        let weightCell = collectionView.dequeueReusableCellWithReuseIdentifier(MainStoryboard.CollectionViewCellIdentifiers.weightCell, forIndexPath: indexPath)
+
+        weightCell.backgroundColor = UIColor.redColor()
+
+        return weightCell
+    }
 }
 
