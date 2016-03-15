a0b668edcfcf2ebbc03933be75f34b6b8c79e64d
Connor Krupp <connorkrupp@icloud.com>
2016-03-15 16:34:57 -0400

    Added Taskability Framework

---
+++ Taskability/TaskabilityKit/TaskItem.swift
@@ -0,0 +1,45 @@
+//
+//  TaskItem.swift
+//  Taskability
+//
+//  Created by Connor Krupp on 15/03/2016.
+//  Copyright © 2016 Connor Krupp. All rights reserved.
+//
+
+import Foundation
+
+public struct TaskItem {
+
+    // MARK: Properties
+
+    /// Title and subtitle describing the task
+    public var title: String
+    public var subtitle: String?
+
+    /// When is this task your main focus?
+    public var startDate: NSDate?
+    public var endDate: NSDate?
+
+    /// String representation of task location
+    public var location: String?
+
+    /// Array of reminder dates for push notifications
+    public var reminders = [NSDate]()
+
+    /// Is this item completed?
+    public var isComplete = false
+
+    /// Unique Identifier
+    private var UUID = NSUUID()
+
+    /**
+     Initializes a TaskItem instance with designated title
+
+      - parameter title: Title of this TaskItem
+    */
+
+    public init(withTitle title: String) {
+        self.title = title
+    }
+
+}
---
+++ Taskability/TaskabilityKit/TaskabilityKit.h
@@ -0,0 +1,19 @@
+//
+//  TaskabilityKit.h
+//  TaskabilityKit
+//
+//  Created by Connor Krupp on 15/03/2016.
+//  Copyright © 2016 Connor Krupp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TaskabilityKit.
+FOUNDATION_EXPORT double TaskabilityKitVersionNumber;
+
+//! Project version string for TaskabilityKit.
+FOUNDATION_EXPORT const unsigned char TaskabilityKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TaskabilityKit/PublicHeader.h>
+
+
